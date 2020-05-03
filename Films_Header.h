526d9f022b6452e4cff279e263155c89c1b4e8f5
Bryushinin <64682282+Bryushinin@users.noreply.github.com>
2020-05-03 11:23:48 +0300

    Add files via upload

---
+++ Films_Header.h
@@ -0,0 +1,21 @@
+#ifndef FILMS_HEADER_H_INCLUDED
+#define FILMS_HEADER_H_INCLUDED
+
+#include <stdio.h>
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <windows.h>
+#include <fstream>
+#include <algorithm>
+
+using namespace std;
+
+int Compare(string a, string b);
+
+void Razval(char* st);
+
+void Shozhdenie(char* st);
+
+
+#endif // FILMS_HEADER_H_INCLUDED
